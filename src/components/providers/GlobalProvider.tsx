8788c46e81962cce8d2e6a7caa4549c17b7d404e
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2025-08-31 05:34:49 +0100

    feat: fixes

--- src/components/providers/GlobalProvider.tsx
+++ src/components/providers/GlobalProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { type PropsWithChildren } from 'react';
+import React, { Suspense, type PropsWithChildren } from 'react';
 import Navbar from '../global/Navbar';
 import Sidebar from '../global/Sidebar';
 import ModalProvider from './ModalProvider';
@@ -13,29 +13,31 @@ export default function GlobalProvider({ children }: PropsWithChildren) {
   const queryClient = new QueryClient();
 
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      forcedTheme="dark"
-      enableSystem={false}
-      disableTransitionOnChange
-    >
-      <QueryClientProvider client={queryClient}>
-        <ModalProvider>
-          <main className="md:w-[calc(100vw-64px)] md:ml-16">
-            <aside>
-              <Sidebar />
-            </aside>
+    <Suspense>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        forcedTheme="dark"
+        enableSystem={false}
+        disableTransitionOnChange
+      >
+        <QueryClientProvider client={queryClient}>
+          <ModalProvider>
+            <main className="md:w-[calc(100vw-64px)] md:ml-16">
+              <aside>
+                <Sidebar />
+              </aside>
 
-            <aside>
-              <Navbar />
-              <div>{children}</div>
-            </aside>
-          </main>
-          <FilterComponent />
-          <CustomSidebar />
-        </ModalProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
+              <aside>
+                <Navbar />
+                <div>{children}</div>
+              </aside>
+            </main>
+            <FilterComponent />
+            <CustomSidebar />
+          </ModalProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </Suspense>
   );
 }
