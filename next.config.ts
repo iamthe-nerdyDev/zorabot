71ef1efdf4ab56206320996b71ce52e111f40026
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2025-09-01 01:42:34 +0100

    feat: fixes

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination:
-          'https://api.farcaster.xyz/miniapps/hosted-manifest/019902b0-c47b-3c55-2b7c-43f08e083ca6',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
