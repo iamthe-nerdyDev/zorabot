1d6ffac759bee238e1ec302e524d833a15d579eb
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2025-09-01 02:15:36 +0100

    feat: fixes

--- src/components/providers/GlobalProvider.tsx
+++ src/components/providers/GlobalProvider.tsx
@@ -9,9 +9,22 @@ import { ThemeProvider } from './ThemeProvider';
 import FilterComponent from '../global/FilterComponent';
 import CustomSidebar from '../global/CustomSidebar';
 import Modal from '../global/Modal';
+import { sdk } from '@farcaster/miniapp-sdk';
 
 export default function GlobalProvider({ children }: PropsWithChildren) {
+  const [isReady, setIsReady] = React.useState(false);
   const queryClient = new QueryClient();
+
+  React.useEffect(() => {
+    async function init() {
+      if (isReady) return;
+      // --
+      await sdk.actions.ready();
+      setIsReady(true);
+    }
+
+    init();
+  }, [isReady]);
 
   return (
     <Suspense>
