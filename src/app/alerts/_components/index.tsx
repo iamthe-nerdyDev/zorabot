12c0e90e4f4896de8095884fece76589666104fc
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2025-09-02 12:54:25 +0100

    feat: fixes

--- src/app/alerts/_components/index.tsx
+++ src/app/alerts/_components/index.tsx
@@ -1,5 +1,13 @@
+import { Bell } from 'lucide-react';
 import React from 'react';
 
 export default function AlertComponent() {
-  return <div></div>;
+  return (
+    <div className="h-[60dvh]">
+      <div className="h-full flex flex-col items-center justify-center gap-2.5">
+        <Bell className="size-10 opacity-60" strokeWidth={1} />
+        <p className="text-xl opacity-80 text-center">No alert(s) set yet!</p>
+      </div>
+    </div>
+  );
 }
--- src/app/watchlist/_components/index.tsx
+++ src/app/watchlist/_components/index.tsx
@@ -1,7 +1,15 @@
 'use client';
 
+import { Star } from 'lucide-react';
 import React from 'react';
 
 export default function WatchlistComponent() {
-  return <div></div>;
+  return (
+    <div className="h-[60dvh]">
+      <div className="h-full flex flex-col items-center justify-center gap-2.5">
+        <Star className="size-10 opacity-60" strokeWidth={1} />
+        <p className="text-xl opacity-80 text-center">Nothing in watchlist!</p>
+      </div>
+    </div>
+  );
 }
